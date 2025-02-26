59d7308f14f8d95219ccd88106e2f7f7c4c719f9
Malick Ouedraogo <C00400598@louisiana.edu>
2025-02-26 14:23:57 -0600

    gh pages config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import chatbotImg from "/assets/chatbot.png";
+import chatbotImg from "../public/assets/chatbot.png";
 import "./App.css";
 import Chat from "./Components/Chat/Chat";
 import Controls from "./Components/Controls/Controls";
